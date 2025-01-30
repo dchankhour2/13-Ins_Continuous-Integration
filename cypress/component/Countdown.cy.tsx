5f8e99af00749dcfac58491216394ea0694a6858
Dani Chankhour <dani@webperformanceusa.com>
2025-01-29 18:28:44 -0800

    break the tests

--- cypress/component/Countdown.cy.tsx
+++ cypress/component/Countdown.cy.tsx
@@ -5,7 +5,7 @@ describe('<Countdown />', () => {
   it('should initially render with a maximum of 9 guesses', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<Countdown guesses={9}/>)
-    cy.get('[data-cy="countdown"]').should('exist').and('have.text', 'Guesses Remaining: 9')
+    cy.get('[data-cy="countdown"]').should('exist').and('have.text', 'Guesses Remaining: 19')
   })
 
   it('should render with a different number of guesses', () => {
